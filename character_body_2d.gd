08d29301b2395afd66b92acdbd28833f3429d09b
nihilistic0 <a.huang1239@gmail.com>
2025-02-13 15:55:40 -0600

    edits to tilemap

--- character_body_2d.gd
+++ character_body_2d.gd
@@ -39,7 +39,7 @@ func _on_area_2d_showslime() -> void:
 
 
 func _on_big_slime_hideslime(x1, none, y1, none2) -> void:
-	position =Vector2(x1, y1)
+	position = Vector2(x1, y1)
 	visible = true
 	process_mode = PROCESS_MODE_INHERIT
 
