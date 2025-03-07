4b7d1b0bfd0e552abe754dcabbd03aadc64bb2dc
nihilistic0 <a.huang1239@gmail.com>
2025-03-07 10:35:39 -0600

    changes

--- exit.gd
+++ exit.gd
@@ -1,6 +1,5 @@
 extends Area2D
-signal next_level
-
+var exit = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -11,6 +10,8 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	if len(get_overlapping_areas()) == 4:
-		next_level.emit()
-	if len(get_overlapping_bodies()) == 1 and get_overlapping_bodies()[0].get_collision_layer_value(17):
-		next_level.emit()
+		exit = true
+	elif len(get_overlapping_bodies()) == 1 and get_overlapping_bodies()[0].get_collision_layer_value(17):
+		exit = true
+	else:
+		exit = false
--- global_var.gd
+++ global_var.gd
@@ -3,15 +3,9 @@ var level = 1
 var current_level = 1
 var max_level = 10
 var exit_unconnected = true
-
+var exit = null
 # Called when the node enters the scene tree for the first time.
 
-func _process(delta: float) -> void:
-	if exit_unconnected:
-		var exit = get_node("/root/level_" + str(current_level) + "/LevelMap/Exit")
-		if exit != null:
-			exit.next_level.connect(self._on_level_pass)
-			exit_unconnected = false
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _ready():
@@ -21,7 +15,6 @@ func _ready():
 	load_level()
 
 func load_level():
-
 	var save_file = FileAccess.open("user://savefile.save", FileAccess.READ)
 	if not FileAccess.file_exists("user://savefile.save"):
 		print("Aborting, no savefile")
@@ -37,17 +30,22 @@ func save_level():
 	save_file.store_line(str(level))
 	save_file.close()
 
-func _on_level_pass():
-	if current_level == level:
-		level += 1
-		current_level += 1
-		save_level()
-		if level > max_level:
-			pass
+func _input(ev):
+	if Input.is_action_pressed("ui_select") and get_node("/root/level_" + str(current_level) + "/LevelMap/Exit").exit:
+		if current_level == level:
+			level += 1
+			current_level += 1
+			save_level()
+			if level > max_level:
+				pass
+			else:
+				print("loading level" + str(current_level))
+				get_tree().change_scene_to_file("res://level_" + str(current_level) + ".tscn")
+				exit_unconnected = true
+				exit = null
 		else:
+			print("loading level" + str(current_level))
+			current_level += 1
 			get_tree().change_scene_to_file("res://level_" + str(current_level) + ".tscn")
 			exit_unconnected = true
-	else:
-		current_level += 1
-		get_tree().change_scene_to_file("res://level_" + str(current_level) + ".tscn")
-		exit_unconnected = true
+			exit = null
