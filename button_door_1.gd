1b2be9423d3c35b1185afa8b7f42e301bbd58fca
nihilistic0 <a.huang1239@gmail.com>
2025-03-09 19:03:16 -0500

    things working now yayyyy

--- button_door_1.gd
+++ button_door_1.gd
@@ -1,15 +1,22 @@
 extends Node2D
-@onready var path_follow : PathFollow2D = $LevelMap/ButtonDoor1/Path2D/PathFollow2D
-@export var speed = 100
-@export var button_pressed = true
+@onready var path_follow : PathFollow2D = $Path2D/PathFollow2D
+@export var speed = 0.5
+@export var button_pressed = false
 # Called when the node enters the scene tree for the first time.
-
+func _ready() -> void:
+	$Sprite2D.frame = 0
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	if button_pressed:
-		path_follow.progress += speed*delta
+		if path_follow.progress_ratio + speed*delta < 0.5:
+			path_follow.progress_ratio += speed*delta
+		else:
+			path_follow.progress_ratio = 0.5
 
 
-func _on_area_2d_body_shape_entered(body_rid: RID, body: Node2D, body_shape_index: int, local_shape_index: int) -> void:
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	
 	button_pressed = true
+	$Sprite2D.frame = 5
