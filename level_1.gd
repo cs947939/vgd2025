bf51e4f7e33bf2537ce1a87cbd6f358c7ea9fcf6
nihilistic0 <a.huang1239@gmail.com>
2025-03-10 10:46:51 -0500

    commit

--- level_1.gd
+++ level_1.gd
@@ -1,7 +1,7 @@
 extends Node2D
 
 var mynode = preload("res://players.tscn")
-var spawns = [Vector2(0, -100), Vector2(0, -100), Vector2(0, -100), Vector2(0, -50), Vector2(0, -100)]
+var spawns = [Vector2(0, -100), Vector2(0, -100), Vector2(0, 0), Vector2(0, -50), Vector2(0, -100), Vector2(0, 0)]
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
