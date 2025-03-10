85ac5e9caa291384e21c7a98bc11c21fba8bdb97
nihilistic0 <a.huang1239@gmail.com>
2025-03-10 08:54:44 -0500

    AAA

--- global_var.gd
+++ global_var.gd
@@ -28,23 +28,19 @@ func save_level():
 	save_file.store_line(str(level))
 	save_file.close()
 func _process(delta: float) -> void:
-	if Input.is_action_pressed("ui_select") and input_allowed and get_node("/root/level_" + str(current_level) + "/LevelMap/Exit").exit:
+	if Input.is_action_pressed("ui_select") and input_allowed and get_node("/root/level_" + str(current_level) + "/LevelMap/Exit") and get_node("/root/level_" + str(current_level) + "/LevelMap/Exit").exit:
 		input_allowed = false
 		if current_level == level:
 			level += 1
 			current_level += 1
-			save_level()
+			save_level()  
 			if level > max_level:
 				pass
 			else:
 				print("loading level" + str(current_level))
 				get_tree().change_scene_to_file("res://level_" + str(current_level) + ".tscn")
-				exit_unconnected = true
-				exit = null
 		else:
 			print("loading level" + str(current_level))
 			current_level += 1
-			exit_unconnected = true
-			exit = null
 			get_tree().change_scene_to_file("res://level_" + str(current_level) + ".tscn")
 		input_allowed = true
--- level_1.gd
+++ level_1.gd
@@ -1,10 +1,12 @@
 extends Node2D
 
 var mynode = preload("res://players.tscn")
+var spawns = [Vector2(0, -100), Vector2(0, -100), Vector2(0, -100), Vector2(0, -50), Vector2(0, -100)]
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	var instance = mynode.instantiate()
+	instance.position = spawns[GlobalVar.current_level - 1]
 	add_child(instance)
 
 
