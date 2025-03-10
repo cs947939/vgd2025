9074e70cb547d6521d550376c532a567bce73cf2
nihilistic0 <a.huang1239@gmail.com>
2025-03-09 19:45:28 -0500

    stuff

--- exit.gd
+++ exit.gd
@@ -3,7 +3,7 @@ var exit = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	$Label.process_mode = Node.PROCESS_MODE_DISABLED
 
 
 		
@@ -11,7 +11,13 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	if len(get_overlapping_areas()) == 4:
 		exit = true
+		$Label.process_mode = Node.PROCESS_MODE_INHERIT
 	elif len(get_overlapping_bodies()) == 1 and get_overlapping_bodies()[0].get_collision_layer_value(17):
 		exit = true
+		$Label.process_mode = Node.PROCESS_MODE_INHERIT
 	else:
 		exit = false
+		$Label.process_mode = Node.PROCESS_MODE_DISABLED
+	if exit:
+		$Label.modulate.a = (sin(Time.get_unix_time_from_system())**2)/2+1/2
+	
