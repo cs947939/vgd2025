4295ef8599dbf5543d2234559326841994567ade
nihilistic0 <a.huang1239@gmail.com>
2025-03-12 10:22:53 -0500

    Update level_1.gd

--- level_1.gd
+++ level_1.gd
@@ -1,7 +1,7 @@
 extends Node2D
 
 @onready var mynode = preload("res://players.tscn")
-@onready var spawns = [Vector2(0, -100), Vector2(0, -100), Vector2(0, 0), Vector2(0, -50), Vector2(0, -100), Vector2(0, -50)]
+@onready var spawns = [Vector2(0, -100), Vector2(0, -100), Vector2(0, 0), Vector2(0, -50), Vector2(0, -50), Vector2(0, -100)]
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
