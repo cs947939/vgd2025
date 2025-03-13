e476e2377eaabbce08522ffb30070aae57113693
nihilistic0 <a.huang1239@gmail.com>
2025-03-12 19:40:33 -0500

    asa

--- button.gd
+++ button.gd
@@ -2,6 +2,7 @@ extends Button
 @onready var enabled = false
 @onready var passed = false
 @onready var names = ["Button", "Button2", "Button3", "Button4", "Button5", "Button6"]
+@onready var shadow_names = [$"../Sprite2D", $"../Sprite2D2", $"../Sprite2D3", $"../Sprite2D4", $"../Sprite2D5", $"../Sprite2D6"]
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -11,6 +12,11 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	if enabled:
 		self.disabled = false
+	if button_pressed:
+		shadow_names[names.find(self.name)].visible = false
+	else:
+		shadow_names[names.find(self.name)].visible = true
+		
 		
 
 
--- main_menu.gd
+++ main_menu.gd
@@ -8,4 +8,44 @@ func _process(delta: float) -> void:
 	$Instructions.rotation = PI/24*(sin(Time.get_unix_time_from_system()/5))
 	$Instructions.scale.x = 1+0.05*sin(Time.get_unix_time_from_system()*sqrt(2)/5)
 	$Instructions.scale.y = 1+0.05*sin(Time.get_unix_time_from_system()*sqrt(2)/5)
-	$Instructions.modulate.a = 0.75+0.25*sin(Time.get_unix_time_from_system()*sqrt(3)/5)
+	$Instructions.modulate.a = 0.9+0.1*sin(Time.get_unix_time_from_system()*sqrt(3)/5)
+	if $PlayerSelect/Button2.button_pressed:
+		$PlayerSelect/Sprite2D2.visible = false
+	else:
+		$PlayerSelect/Sprite2D2.visible = true
+	if $PlayerSelect/Button3.button_pressed:
+		$PlayerSelect/Sprite2D3.visible = false
+	else:
+		$PlayerSelect/Sprite2D3.visible = true
+	if $PlayerSelect/Button4.button_pressed:
+		$PlayerSelect/Sprite2D4.visible = false
+	else:
+		$PlayerSelect/Sprite2D4.visible = true
+
+
+func _on_button_2_pressed() -> void:
+	GlobalVar.players = 2
+	$PlayerSelect.visible = false
+	$Instructions/Label2.visible = false
+	$Instructions/Label.visible = true
+	$Levels_Button.visible = true
+	$Play_Button.visible = true
+
+
+
+func _on_button_3_pressed() -> void:
+	GlobalVar.players = 3
+	$PlayerSelect.visible = false
+	$Instructions/Label2.visible = false
+	$Instructions/Label.visible = true
+	$Levels_Button.visible = true
+	$Play_Button.visible = true
+
+
+func _on_button_4_pressed() -> void:
+	GlobalVar.players = 4
+	$PlayerSelect.visible = false
+	$Instructions/Label2.visible = false
+	$Instructions/Label.visible = true
+	$Levels_Button.visible = true
+	$Play_Button.visible = true
